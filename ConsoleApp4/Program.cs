aeed28d730bcfd0585c43f77350cb0d14e6dae9f
Hao0000 <bim.bim39999@gmail.com>
2021-11-16 11:54:58 +0700

    Nhap_xuat

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,70 +1,71 @@
 ﻿using System;
 
-namespace Congthuctoan
+namespace Nhap_xuat
 {
-    class canbanhac
+    class nhap_xuat
     {
         static void Main(string[] args)
         {
-            float a, b, c;
-            string stra, strb, strc;
-            
-            Console.Write("\nTinh phuong trinh bac 2");
-            Nhap_lai://trong truong hop nhap sai
-            Console.Write("\n -------------------------\n");
-            Console.Write("\n\n Nhap ba bien a, b, c");
-            Console.Write("\n Nhap a: ");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            stra = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            Console.Write("\n Nhap b: ");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            strb = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            Console.Write("\n Nhap c: ");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            strc = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            Console.Write("\n");
-            if (float.TryParse(stra, out a) == false|| float.TryParse(strb, out b) == false || float.TryParse(strc, out c) == false)
-            {
-                Console.Write("\n Gia tri vua nhap khong phai so\n");
-                Console.Write("\n Moi ban nhap lai");
-                goto Nhap_lai;
-            }
-            else
-            {
-                if(a==0)//Phuong trinh bac hai a phai khac 0
-                {
-                    Console.Write("\n Gia tri a phai khac 0\n");
-                    Console.Write("\n Moi ban nhap lai");
-                    goto Nhap_lai ;
-                  
-                }
-                float delta = (b * b) - (4 * a * c);
-                Console.Write("\n Gia tri cua delta la : " + delta+"\n");
-                if (delta == 0)
-                {
-                    float x = (-b) / 2 * a;
-                    Console.Write("\nPhuong trinh co nghiem kep\n");
-                    Console.Write("\nx1 = x2 = " + x);
+            Console.Write("Chuong trinh nhap xuat thong tin ung vien");
+            Console.Write("\n=======================================");
+            Console.Write("\n***Moi nhap ung vien so 1:");
+            Console.Write("\n   +Nhap ho ten: ");
+            Console.Write("\n   +Nhap nam sinh: ");
+            Console.Write("\n   +Nhap gioi tinh: ");
+            Console.Write("\n   +Nhap chieu cao: ");
+            Console.Write("\n   +Nhap can nang: ");
+            Console.Write("\n   +Nhap que quan: ");
+            Console.Write("\n   +Nhap chuyen nganh: ");
+            Console.Write("\n   +Nhap diem TB: ");
+            Console.Write("\n***Moi nhap ung vien so 2:");
+            Console.Write("\n   +Nhap ho ten: ");
+            Console.Write("\n   +Nhap nam sinh: ");
+            Console.Write("\n   +Nhap gioi tinh: ");
+            Console.Write("\n   +Nhap chieu cao: ");
+            Console.Write("\n   +Nhap can nang: ");
+            Console.Write("\n   +Nhap que quan: ");
+            Console.Write("\n   +Nhap chuyen nganh: ");
+            Console.Write("\n   +Nhap diem TB: ");
+            Console.Write("\n***Moi nhap ung vien so 3:");
+            Console.Write("\n   +Nhap ho ten: ");
+            Console.Write("\n   +Nhap nam sinh: ");
+            Console.Write("\n   +Nhap gioi tinh: ");
+            Console.Write("\n   +Nhap chieu cao: ");
+            Console.Write("\n   +Nhap can nang: ");
+            Console.Write("\n   +Nhap que quan: ");
+            Console.Write("\n   +Nhap chuyen nganh: ");
+            Console.Write("\n   +Nhap diem TB: ");
+            Console.Write("\n=======================================");
+            Console.Write("\nDanh sach ung cu vien so da nhap");
+            Console.Write("\n=======================================");
+            Console.Write("\n***Ung cu vien so 1");
+            Console.Write("\n   +Ho ten: Nguyen Van A");
+            Console.Write("\n   +Nam sinh: 1998");
+            Console.Write("\n   +Gioi tinh: F ");
+            Console.Write("\n   +Chieu cao: 1.62 ");
+            Console.Write("\n   +Can nang: 52 ");
+            Console.Write("\n   +Que quan: TP HCM");
+            Console.Write("\n   +Chuyen nganh: Thiet ke ");
+            Console.Write("\n   +Diem TB: 7.5 ");
+            Console.Write("\n***Ung cu vien so 2");
+            Console.Write("\n   +Ho ten: Tran The B");
+            Console.Write("\n   +Nam sinh: 1996");
+            Console.Write("\n   +Gioi tinh: M ");
+            Console.Write("\n   +Chieu cao: 1.72 ");
+            Console.Write("\n   +Can nang: 65 ");
+            Console.Write("\n   +Que quan: HA NOI");
+            Console.Write("\n   +Chuyen nganh: Game dev ");
+            Console.Write("\n   +Diem TB: 8.1 ");
+            Console.Write("\n***Ung cu vien so 3");
+            Console.Write("\n   +Ho ten: Le Hoang C");
+            Console.Write("\n   +Nam sinh: 1997");
+            Console.Write("\n   +Gioi tinh: M ");
+            Console.Write("\n   +Chieu cao: 1.68 ");
+            Console.Write("\n   +Can nang: 57 ");
+            Console.Write("\n   +Que quan: Binh Duong");
+            Console.Write("\n   +Chuyen nganh: LT Web ");
+            Console.Write("\n   +Diem TB: 7.8 ");
 
-                }
-                else if (delta > 0)
-                {
-                    double x1 = (-b + Convert.ToDouble(Math.Sqrt(delta))) / 2 * a;
-                    double x2 = (-b - Convert.ToDouble(Math.Sqrt(delta))) / 2 * a;
-                    Console.Write("\nPhuong trinh co hai nghiem\n");
-                    Console.Write("\nx1 = " + x1);
-                    Console.Write("\nx2 = " + x2);
-                }
-                else
-                {
-                    Console.Write("\nPhuong trinh vo nghiem");
-
-                }
-            }
-            
 
         }
     }
