159c467fe02260caaae46b38e53665a5b5f8914a
Hao0000 <bim.bim39999@gmail.com>
2021-11-19 10:25:25 +0700

    Bai tap luu do 1-a

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,97 +1,25 @@
 ﻿using System;
 
-namespace Nhap_xuat
+namespace Luudo
 {
-    class nhap_xuat
+    class baitap
     {
         static void Main(string[] args)
         {
-            String name, addr, sex, hight, weitght, born, sub, test,date, name1, addr1, sex1, hight1, weitght1, born1, sub1, test1, date1, name2, addr2, sex2, hight2, weitght2, born2, sub2, test2, date2;
-            Console.Write("Chuong trinh nhap xuat thong tin ung vien");
-            Console.Write("\n=======================================");
-            Console.Write("\n***Moi nhap ung vien so 1:");
-            Console.Write("\n   +Nhap ho ten: ");
-            name = Console.ReadLine();
-            Console.Write("\n   +Nhap nam sinh: ");
-            date = Console.ReadLine();
-            Console.Write("\n   +Nhap gioi tinh: ");
-            sex = Console.ReadLine();
-            Console.Write("\n   +Nhap chieu cao: ");
-            hight = Console.ReadLine();
-            Console.Write("\n   +Nhap can nang: ");
-            weitght = Console.ReadLine();
-            Console.Write("\n   +Nhap que quan: ");
-            born = Console.ReadLine();
-            Console.Write("\n   +Nhap chuyen nganh: ");
-            sub = Console.ReadLine();
-            Console.Write("\n   +Nhap diem TB: ");
-            test = Console.ReadLine();
-            Console.Write("\n***Moi nhap ung vien so 2:");
-            Console.Write("\n   +Nhap ho ten: ");
-            name1 = Console.ReadLine();
-            Console.Write("\n   +Nhap nam sinh: ");
-            date1 = Console.ReadLine();
-            Console.Write("\n   +Nhap gioi tinh: ");
-            sex1 = Console.ReadLine();
-            Console.Write("\n   +Nhap chieu cao: ");
-            hight1 = Console.ReadLine();
-            Console.Write("\n   +Nhap can nang: ");
-            weitght1 = Console.ReadLine();
-            Console.Write("\n   +Nhap que quan: ");
-            born1 = Console.ReadLine();
-            Console.Write("\n   +Nhap chuyen nganh: ");
-            sub1 = Console.ReadLine();
-            Console.Write("\n   +Nhap diem TB: ");
-            test1 = Console.ReadLine();
-            Console.Write("\n***Moi nhap ung vien so 3:");
-            Console.Write("\n   +Nhap ho ten: ");
-            name2 = Console.ReadLine();
-            Console.Write("\n   +Nhap nam sinh: ");
-            date2 = Console.ReadLine();
-            Console.Write("\n   +Nhap gioi tinh: ");
-            sex2 = Console.ReadLine();
-            Console.Write("\n   +Nhap chieu cao: ");
-            hight2 = Console.ReadLine();
-            Console.Write("\n   +Nhap can nang: ");
-            weitght2 = Console.ReadLine();
-            Console.Write("\n   +Nhap que quan: ");
-            born2 = Console.ReadLine();
-            Console.Write("\n   +Nhap chuyen nganh: ");
-            sub2 = Console.ReadLine();
-            Console.Write("\n   +Nhap diem TB: ");
-            test2 = Console.ReadLine();
-            Console.Write("\n=======================================");
-            Console.Write("\nDanh sach ung cu vien so da nhap");
-            Console.Write("\n=======================================");
-            Console.Write("\n***Ung cu vien so 1");
-            Console.Write("\n   +Ho ten: " + name);
-            Console.Write("\n   +Nam sinh: " + date);
-            Console.Write("\n   +Gioi tinh: " + sex);
-            Console.Write("\n   +Chieu cao: " + hight);
-            Console.Write("\n   +Can nang: " + weitght);
-            Console.Write("\n   +Que quan: "+ born);
-            Console.Write("\n   +Chuyen nganh: " + sub);
-            Console.Write("\n   +Diem TB: " + test);
-            Console.Write("\n***Ung cu vien so 2");
-            Console.Write("\n   +Ho ten: " + name1);
-            Console.Write("\n   +Nam sinh: " + date1);
-            Console.Write("\n   +Gioi tinh: " + sex1);
-            Console.Write("\n   +Chieu cao: " + hight1);
-            Console.Write("\n   +Can nang: " + weitght1);
-            Console.Write("\n   +Que quan: " + born1);
-            Console.Write("\n   +Chuyen nganh: " + sub1);
-            Console.Write("\n   +Diem TB: " + test1);
-            Console.Write("\n***Ung cu vien so 3");
-            Console.Write("\n   +Ho ten: " + name2);
-            Console.Write("\n   +Nam sinh: " + date2);
-            Console.Write("\n   +Gioi tinh: " + sex2);
-            Console.Write("\n   +Chieu cao: " + hight2);
-            Console.Write("\n   +Can nang: " + weitght2);
-            Console.Write("\n   +Que quan: " + born2);
-            Console.Write("\n   +Chuyen nganh: " + sub2);
-            Console.Write("\n   +Diem TB: " + test2);
-
-
+            float a, b, c;
+            Console.WriteLine("Nhập hai giá trị");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            if(b ==0)
+            {
+                Console.WriteLine("Error Divide to Zero");
+            }    
+            else
+            {
+                c = b / a;
+                Console.WriteLine(c);
+            }   
         }
     }
 }
+    
