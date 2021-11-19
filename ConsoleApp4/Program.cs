6a20ee22fc78b16c69232fba2a6605b1422ff24d
Hao0000 <bim.bim39999@gmail.com>
2021-11-19 10:39:41 +0700

    Bai tap luu do 1-c

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,20 +6,26 @@ namespace Luudo
     {
         static void Main(string[] args)
         {
-            float a, b, c;
-            Console.WriteLine("Nhập hai giá trị");
-            a = float.Parse(Console.ReadLine());
-            b = float.Parse(Console.ReadLine());
-            if(b ==0)
+            float n =float.Parse(Console.ReadLine());
+            float m = 2;
+            float r;
+        Comback:
+            r = n % m;
+            Console.WriteLine("Gia tri r là :" + r);
+            if (r == 0)
             {
-                Console.WriteLine("Error Divide to Zero");
+                Console.WriteLine("N is NOT prime number");
+            }    
+            else if (m < n-1)
+            {
+                Console.WriteLine("N is a Prime Number");
             }    
             else
             {
-                c = a / b;
-                Console.WriteLine(c);
+                m = m + 1;
+                goto Comback;
+            }    
             }   
         }
     }
-}
     
