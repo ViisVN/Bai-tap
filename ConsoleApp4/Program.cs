91f85f4b92bb64da238ce4805c04c6afb468a743
Hao0000 <bim.bim39999@gmail.com>
2021-11-23 12:00:48 +0700

    bai vonglap 5

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,59 +6,24 @@ namespace Vong_lap
     {
         static void Main(string[] args)
         {
-            int n1;
-            Console.Write("\nSO THICH CA NHAN");
-            Console.Write("\n================");
-            Console.Write("\n1. Doc sach");
-            Console.Write("\n2. Nghe nhac");
-            Console.Write("\n3. Choi the thao");
-            Console.Write("\n4. May tinh");
-            Console.Write("\n5. Thoat");
-            Console.Write("\n================");
-            Console.Write("\nChon: ");
-            string n = Console.ReadLine();
-            if (int.TryParse(n, out n1) == true)
+            Console.Write("Nhap 1 so nguyen lon hon 0:  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[Convert.ToString(n).Length];
+            int sum = 0;
+            for (int i = 0; i < Convert.ToString(n).Length; i++)
             {
-                while (int.TryParse(n, out n1) == true)
-                {
-                    if (Convert.ToInt32(n) == 1)
-                    {
-                        Console.Write("Ban thich doc sach");
-                        break;
-                    }
-                    else if (Convert.ToInt32(n) == 2)
-                    {
-                        Console.Write("Ban thich nghe nhac");
-                        break;
-                    }
-                    else if (Convert.ToInt32(n) == 3)
-                    {
-                        Console.Write("Ban thich choi the thao");
-                        break;
-                    }
-                    else if (Convert.ToInt32(n) == 4)
-                    {
-                        Console.Write("Ban thich may tinh");
-                        break;
-                    }
-                    else if (Convert.ToInt32(n) == 5)
-                    {
-                        Console.Write("Hen gap lai");
-                        break;
-                    }
-                    else if (Convert.ToInt32(n) < 1 || Convert.ToInt32(n) > 5)
-                    {
-                        Console.Write("Ban nhap sai");
-                        break;
-                    }
-                }
-              }
-             else
-              {
-                Console.Write("\nBan da nhap sai");
-              }
+                arr[i] = (Convert.ToInt32(Convert.ToString(n)[i]) - 48);
             }
-        }
-    }
+            Console.Write("\nDay so bao gom: ");
+            for (int i = 0; i < Convert.ToString(n).Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                sum += arr[i];
+            }
+            Console.Write("\nTong cac so trong do la: " + sum);
 
+        }
+
+    }
+}
     
