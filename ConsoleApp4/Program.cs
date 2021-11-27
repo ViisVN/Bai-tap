176d3aa116cf296d66f6a03e7babfb62cb306536
Hao0000 <bim.bim39999@gmail.com>
2021-11-27 11:25:27 +0700

    Bai tap mang 2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,19 +6,26 @@ namespace String
     {
         static void Main(string[] args)
         {
-            Console.Write("\n Nhap vao so luong phan tu: "); int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Nhap vao so luong phan tu: "); int n = 10;
             int[] a = new int[n];
-            Console.Write("\n Cac so nam trong n gom: ");
-            for (int i=0; i<n;i++)
+            Console.Write("\n Nhap vao so luong phan tu: "); int m = 5;
+            double[] b = new double[m];
+            for (int i=0; i<m;i++)
             {
-                Console.Write("\na[" + i + "]");
-                a[i]=Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nb[" + i + "]: ");
+                b[i]=Convert.ToDouble(Console.ReadLine());
             }
-            Console.Write("\nPhan tu nam o day chan la: ");
-            for(int i=0; i<n;i+=2)
+            for (int i=0; i < m ;i++)
             {
-                Console.Write("\na[" + i + "]");
-                a[i] = Convert.ToInt32(Console.ReadLine());
+                if(i%2!=0)
+                {
+                   a[i] = (int)b[i];
+                }
+            }
+            Console.Write("\nMang a gom: ");
+            for(int i=0; i<n ;i++)
+            {
+                Console.Write("\na[" + i + "] :" + a[i]);
             }    
         }
     }
