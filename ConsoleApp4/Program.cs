f382ad0bffefa197391ed6e61cf0d3cc51c67090
Hao0000 <bim.bim39999@gmail.com>
2021-11-30 12:21:54 +0700

    Bai tap mang 4

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,16 +4,39 @@ namespace String
 {
     class Program
     {
-        static void fina(long d)
+        static void Diem(float a)
         {
-          double b = (1 / Math.Sqrt(5) * ((Math.Pow(((1 + Math.Sqrt(5)) / 2), d)) - (Math.Pow(((1 - Math.Sqrt(5)) / 2), d))));
-            Console.WriteLine("Gia tri so fina thu "+d+" la : "+(int)b);
+            if(a>=0&&a<=3.9)
+            {
+                Console.Write("F");
+            }    
+            else if(a>=4.0&&a<=5.4)
+            {
+                Console.Write("D");
+            }
+            else if (a >= 5.5 && a <= 6.9)
+            {
+                Console.Write("C");
+            }
+            else if (a >= 7.0 && a <= 8.4)
+            {
+                Console.Write("B");
+            }
+            else if (a >= 8.5 && a <= 10)
+            {
+                Console.Write("A");
+            }
+            else
+            {
+                Console.Write("E");
+            }    
         }
         static void Main(string[] args)
         {
-            Console.Write("\nNhap thu tu so fina: ");
-            int a= Convert.ToInt32(Console.ReadLine());
-            fina(a);
+            Console.Write("\nNhap diem: ");
+            float a= float.Parse(Console.ReadLine());
+            Console.Write("Thu hang la: ");
+            Diem(a);
         }
       
     }
