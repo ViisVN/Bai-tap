5e67463181d8195bcee9c0197e19650784396341
Hao0000 <bim.bim39999@gmail.com>
2021-11-30 11:44:36 +0700

    Bai 151/1000

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,43 +3,28 @@
 namespace String
 {
     class Program
-    {
-        public static void GiaiPT2(double a, double b, double c)
+    { 
+        static void Diemtb(double a, double b)
         {
-            double delta = (Math.Pow(b, 2)) - (4 * a * c);
-            if (delta < 0)
-            {
-                Console.Write("\nPhuong trinh vo nghiem.");
-            }
-            else if (delta == 0)
-            {
-                double x = -b / a;
-                Console.Write("\nPhuong trinh co nghiem kep x la: "+x);
-                
-            }
-            else
-            {
-                double x1 = (-b + Math.Sqrt(delta)) / (2*a);
-                double x2 = (-b - Math.Sqrt(delta)) / (2*a);
-                Console.Write("\nPhuong trinh co hai nghiem");
-                Console.Write("\nx1: "+x1);
-                Console.Write("\nx2: "+x2);
-            }
+           double c= (a + b) / 2;
+            Console.Write(c);
         }
         static void Main(string[] args)
-        {
-            Console.Write("\nNhap a b c:\n");
-            nhaplai:
-            int a = int.Parse(Console.ReadLine());
-            if (a==0)
-            {
-                Console.Write("\na phai khac khong, nhap lai a\n");
-                goto nhaplai;
-            }    
-            int b = int.Parse(Console.ReadLine());
-            int c = int.Parse(Console.ReadLine());
-            GiaiPT2(a, b, c);
+        {            
+            Console.Write("\n -----------------------------");
+            Console.Write("\n Ho ten   : ");
+            string ten = Console.ReadLine();
+            Console.Write(" Lop      : ");
+            string lop = Console.ReadLine();
+            Console.Write(" Diemtoan : ");
+            int diemtoan = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Diemvan  : ");
+            int diemvan = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Diemtb   : ");
+            Diemtb(diemtoan, diemvan);
+            Console.Write("\n -----------------------------");
         }
+
     }
 }
             
