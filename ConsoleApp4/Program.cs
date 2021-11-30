aab972b773437f264c84add930eaea0d94a2ddfa
Hao0000 <bim.bim39999@gmail.com>
2021-11-30 09:27:37 +0700

    Bai tap mang 6

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,34 +6,29 @@ namespace String
     {
         static void Main(string[] args)
         {
-            int n = 10;
+            int n = 20;
             int[] a = new int[n];
-            for (int i = 0; i < n; i++)
+            Random r = new Random();
+            for (int i = 0; i < n; i++) 
             {
-                Console.Write("\na[" + i + "]: ");
-                a[i] = Convert.ToInt32(Console.ReadLine());
+                a[i] = r.Next(1, 1000);
+                Console.Write("\na[" + i + "]: "+a[i]);
+                
             }
-            Console.Write("Nhap x: ");
+            Console.Write("\n Nhap x: ");
             int x = Convert.ToInt32(Console.ReadLine());
-            int vitri=0, xcount=0;
-            for (int i = 0; i < n; i++)
+            Console.Write("\n Cac so chia het cho x la: ");
             {
-                if(x==a[i])
-                    {
-                    vitri = i;
-                    break;
-                }
-            }
-            for (int i = 0; i < n; i++)
-            {
-                if(x==a[i])
+                for (int i = 0; i < n; i++)
                 {
-                    xcount += 1;
+                    if(a[i]%x==0)
+                    {
+                        Console.Write("\na[" + i + "]: "+a[i]);
+                    }
                 }
             }
-            Console.Write("\n Vitri dau tien cua x trong mang la: " + vitri);
-            Console.Write("\n So lan x xuat hien trong mang la: " + xcount)
-;           
+          
+           
         }
     }
 }
