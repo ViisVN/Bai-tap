350b364c7960d9d65348b00cdf19f8c7e2b97ef4
Hao0000 <bim.bim39999@gmail.com>
2021-12-04 11:55:52 +0700

    bai ham 5

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,72 +4,40 @@ namespace String
 {
     class Program
     {
-        static void menu(string n)
+        static void lietke(int n)
         {
-           
-                switch (Convert.ToInt32(n))
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
                 {
-                    case 1:
-                        Console.Write("| Doing menu 1......                                                                                                   |\n"); break;
-                    case 2:
-                        Console.Write("| Doing menu 2......                                                                                                   |\n"); break;
-                    case 3:
-                        Console.Write("| Doing menu 3......                                                                                                   |\n"); break;
-                    case 4:
-                        Console.Write("| Doing menu 4......                                                                                                   |\n"); break;               
+                    count++;
                 }
-            if (Convert.ToInt32(n) == 5)
-            {
-                        Console.Write("| Exit...............                                                                                                  |\n");
             }
-            else
+                if (count == 2)
+                {
+                    Console.Write(n + " ");
+                }
+            
+        }
+
+            static void Main(string[] args)
             {
-                nhap(n);
+                int[] a = new int[10];
+            Random r = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                a[i] = r.Next(1, 100000);
+                }
+            Console.WriteLine("So nguyen to gom");
+                for (int i = 0; i < 10; i++)
+                {
+                    lietke(a[i]);
+                }
+
             }
         }
-        static void nhap(string n)
-        {
-            Console.Write("|            PLEASE CHOOSE: ");
-            n = Console.ReadLine();
-            if (n.Length>=2)
-            {
-                Console.Write("| Please choose again                                                                                                  |");
-                nhap(n);
 
-            }
-            else 
-            {
-                if (Convert.ToChar(n) >= 'a' && Convert.ToChar(n) <= 'z' || Convert.ToChar(n) >= 'A' && Convert.ToChar(n) <= 'Z' || Convert.ToInt32(n) < 1 || Convert.ToInt32(n) > 5)
-                {
-                    Console.Write("| Please choose again                                                                                                  |");
-                    nhap(n);
-
-                }
-                else
-                {
-                    menu(n);
-                }
-            }    
-        }
-        static void Main(string[] args)
-        {
-            string n="abc";
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------+");
-            Console.WriteLine("|                                                      Menu                                                            |");
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------+");
-            Console.WriteLine("| 1. Menu 1                                                                                                            |");
-            Console.WriteLine("| 2. Menu 2                                                                                                            |");
-            Console.WriteLine("| 3. Menu 3                                                                                                            |");
-            Console.WriteLine("| 4. Menu 4                                                                                                            |");
-            Console.WriteLine("| 5. Exit                                                                                                              |");
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------+");
-            nhap(n);
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------+");
-
-
-        }
     }
-
-}
 
             
