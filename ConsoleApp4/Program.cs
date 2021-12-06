6e869d06872f3c7fcf71f2d2ebef9400145aeec2
Hao0000 <bim.bim39999@gmail.com>
2021-12-06 11:08:54 +0700

    bai ham 6

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,40 +4,46 @@ namespace String
 {
     class Program
     {
-        static void lietke(int n)
+        static void Main(string[] args)
         {
-            int count = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                if (n % i == 0)
-                {
-                    count++;
-                }
-            }
-                if (count == 2)
-                {
-                    Console.Write(n + " ");
-                }
-            
-        }
-
-            static void Main(string[] args)
-            {
-                int[] a = new int[10];
+            int[] n = new int[10];
             Random r = new Random();
-                for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
+            {
+                
+                n[i] = r.Next(1, 30);
+                Console.Write("n[" + i + "]: "+n[i]+"\n");
+            }
+           for (int i = 0; i < 10; i++)
                 {
-                a[i] = r.Next(1, 100000);
-                }
-            Console.WriteLine("So nguyen to gom");
-                for (int i = 0; i < 10; i++)
-                {
-                    lietke(a[i]);
+                    if (cong(n[i]) == n[i])
+                    {
+                        Console.WriteLine(n[i]+" la so hoan hao");
+                    }
                 }
 
             }
-        }
 
+            static int cong(int n)
+            {
+                int[] b = new int[n];
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    b[i] = i + 1;
+                }
+                for (int i = 0; i < (n - 1); i++)
+                {
+                    if (n % b[i] == 0)
+                    {
+                        sum += b[i];
+                    }
+                }
+
+                return sum;
+            }
+        }
     }
+
 
             
