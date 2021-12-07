f47a989836afd42a9a2cdb8f5d10e3b8deb04568
Hao0000 <bim.bim39999@gmail.com>
2021-12-07 10:36:33 +0700

    bai_lab_4 vong_lap_7(dung ham)

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -2,50 +2,67 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        static void socantinh(ref int[] a)
         {
-            int n = 0;
-            int range = Convert.ToInt32(Console.ReadLine());
-            int count = 0;
-            int[] a = new int[short.MaxValue];
-            int[] v = new int[range];
             for (int i = 0; i < short.MaxValue; i++)
             {
                 a[i] = i + 1;
             }
-            for (int i = 0; i< short.MaxValue; i++)
+        }
+        static void tinhsonguyento(int[] a, ref int[] b, int range)
+        {
+            int n = 0;
+            int count = 0;
+            for (int i = 0; i < short.MaxValue; i++)
             {
-                for (int j = 0;j< short.MaxValue; j++)
+                for (int j = 0; j < short.MaxValue; j++)
                 {
-                    if(a[i]%(j+1)==0)
+                    if (a[i] % (j + 1) == 0)
                     {
                         count++;
                     }
                     if (count == 2 && a[i] == (j + 1))
                     {
-                        Console.WriteLine("a[" + i + "] "+a[i]);
-                        v[n] = a[i];
+                        b[n] = a[i];
                         n++;
                         count = 0;
                         break;
                     }
+                    if (count > 2)
+                    {
+                        break;
+                    }
                 }
-                if(count >2)
+                if (count > 2)
                 {
                     count = 0;
                 }
-                if(n==range)
+                if (n == range)
                 {
                     break;
                 }
-            }
-            for(int i = 0; i<range;i++)
-            {
-                Console.WriteLine("v[" + i + "] " + v[i]);
+
             }
         }
-    }
+        static void xuatdulieu(int[] a,int range)
+        {
+            for(int i =0; i<range;i++)
+            {
+                Console.WriteLine("[" + i + "] : " + a[i]);
+            }
+        }
+            static void Main(string[] args)
+            {
+                Console.Write("Nhap so luong so nguyen to ban muon xuat ra: ");
+                int range = Convert.ToInt32(Console.ReadLine());
+                int[] v = new int[range];
+                int[] a = new int[short.MaxValue];  //Tinh den gioi han cua short
+                socantinh(ref a);
+                tinhsonguyento(a, ref v, range);
+                xuatdulieu(v, range);
+            }
+        }
 
-}
+    }
 
 
