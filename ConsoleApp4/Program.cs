70f16fd84e9e33f2cd9a07e2e7aec666d0f81481
Hao0000 <bim.bim39999@gmail.com>
2021-12-21 10:18:43 +0700

    Bai_lab_chuoi Bai_3

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace String
 {
@@ -6,81 +7,43 @@ namespace String
     {
         static void Main(string[] args)
         {
-            string a = null;
-            int m = 1;
-            do
+            string a = "Don't nod";
+            string b = null;
+            string c = null;
+            for (int i = a.Length - 1; i >= 0; i--)
             {
-                Console.WriteLine("------------------------");
-                Console.Write("Nhap chuoi ki tu: ");
-                a = Console.ReadLine();
-                Console.WriteLine("------------------------");
-                Console.WriteLine("1. So luong ki tu");
-                Console.WriteLine("2. So nguyen am phu am");
-                Console.WriteLine("3. So tu");
-                Console.WriteLine("------------------------");
-                m= Convert.ToInt32(Console.ReadLine());
-                switch(m)
-                {
-                    case 1: sokitu(a); break;
-                    case 2: Nguyenphuam(a); break;
-                    case 3: Sotu(a); break;
-                    default: Console.WriteLine("Thoat"); break;
-                }    
-            } while (m >= 1 && m <= 3);
-        }
-        static void sokitu(string a)
-        {
-            int b = 0;
-            for (int i =0; i < a.Length; i++)
-            {
-              
-                if(a[i] !=' ')
-                {
-                    b++;
-                }
-               
+                b += a[i];
             }
-            Console.WriteLine(value: $"So ki tu trong chuoi ({a}) la {b}");
-        }
-        static void Nguyenphuam(string a)
-        {
-            int b = 0; int c = 0;
-            for(int i = 0; i < a.Length; i++)
+            for (int i = 0; i < b.Length; i++)
             {
-                if (a[i] == 'a' || a[i] == 'i' || a[i] == 'u'|| a[i] == 'e' || a[i] == 'o' || a[i] == 'A' || a[i] == 'I' || a[i] == 'O' || a[i] == 'E' || a[i] == 'U')
+
+                for (int j = 65; j <= 90; j++)
                 {
-                    b++;
-                }    
-                else if(a[i]==' ')
-                {
-                    continue;
-                }    
-                else
-                {
-                    c++;
-                }    
-            }
-            Console.WriteLine(value: $"Chuoi ({a}) co {b} nguyen am va {c} phu am");
-        }
-        static void Sotu(string a)
-        {
-            int b = 0;
-            for (int i =0; i< a.Length; i++)
-            {
-                if (a[i] == ' '&&a[i-1]!=' ')
-                {
-                    b++;
+                    if ((int)b[i] == j)
+                    {
+                        b = b.Replace(b[i], (char)(j + 32));
+                    }
                 }
             }
-            if(a[a.Length-1]!=' ')
+            b = b.Replace(" ", "");
+            b = b.Replace("'", "");
+            b = b.Replace(",", "");
+            for (int i = b.Length - 1; i >= 0; i--)
             {
-                b++;
+                c += b[i];
+            }
+            if(b == c)
+            {
+                Console.WriteLine(value: $"{a} are Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(value: $"{a} are not Palindrome");
             }    
-            Console.WriteLine(value: $"So tu co trong chuoi ({a}) la {b}");
         }
     }
-            
- }
+}
+
 
         
 
