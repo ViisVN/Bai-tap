6c85c9cb55076aee2e1a2df2854200640ca85df0
Hao0000 <bim.bim39999@gmail.com>
2021-12-29 12:56:57 +0700

    Lab_Collection Bai_4

--- ConsoleApp4/Class1.cs
+++ ConsoleApp4/Class1.cs
@@ -1,31 +1,44 @@
 ﻿using System;
+using System.Collections;
 
 namespace String
 {
-    public class Bank
+    public class students
     {
 
-        public int stk;
-        public string name;
-        public double balance;
-        public double rate = 0.045;
-        public Bank(int stk1, string name1, int balance1)
+        public string s_code;
+        public string f_name;
+        public string s_name;
+        public string p_name;
+        public int age;
+        public double s_mark;
+        public students(string s_code1, string f_name1, string s_name1, string p_name1, int age1, double s_mark1)
         {
-            stk = stk1;
-            name = name1;
-            balance = balance1;          
+            s_code = s_code1;
+            f_name = f_name1;
+            s_name = s_name1;
+            p_name = p_name1;
+            age = age1;
+            s_mark = s_mark1;
         }
-        public Bank()
+        public students()
         {
 
         }
-        public void guest()
+        public void infor()
             {
             Console.WriteLine("==============================================");
-            Console.WriteLine("TEN KHACH HANG: {0}", name);
-                Console.WriteLine("SO TAI KHOAN: {0}", stk);
-                Console.WriteLine("SO DU: {0}$", balance);
+            Console.WriteLine("TEN HOC SINH: {0} {1} {2} ", f_name,s_name,p_name);
+                Console.WriteLine("MA SO HOC SINH: {0}", s_code);
+                Console.WriteLine("TUOI: {0}", age);
+            Console.WriteLine("DIEM TB: {0}", s_mark);
             Console.WriteLine("==============================================");
+        }
+        public string name()
+        {
+            string name1;
+            name1 = $"{f_name} {s_name} {p_name}";
+            return name1;
         }
         
     }
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,52 +7,210 @@ namespace String
     {
         static void Main(string[] args)
         {
-            Queue<int> a = new Queue<int>();
+            ArrayList a = new ArrayList();
             int m = 1;
             do
             {
-                Console.WriteLine("----------");
-                Console.WriteLine("1. Them 1 phan tu vao stack");
-                Console.WriteLine("2. Lay phan tu dau khoi stack");
-                Console.WriteLine("3. Xem phan tu dau stack");
-                Console.WriteLine("4. Duyet stack");
-                Console.WriteLine("0. Thoat");
+                Console.WriteLine("Chuong trinh danh sach sinh vien su dung Arraylist");
+                Console.WriteLine("===================================");
+                Console.WriteLine("1- Them mot sinh vien vao danh sach");
+                Console.WriteLine("2- Duyet danh sach");
+                Console.WriteLine("3- Tim thong tin mot sinh vien theo ten");
+                Console.WriteLine("4- Sua diem sinh vien");
+                Console.WriteLine("5- Danh sach sinh vien nhan hoc bong");
+                Console.WriteLine("6- Xem diem cuar sinh vien");
+                Console.WriteLine("7- Sap xep giam dan theo diem tb");
+                Console.WriteLine("8- Sap xep giam dan theo diem tb, tang dan theo ten");
+                Console.WriteLine("0- Thoat chuong trinh");
+                Console.WriteLine("===================================");
                 m = Convert.ToInt32(Console.ReadLine());
-                switch (m)
+                switch(m)
                 {
-                    case 1: Add(a); break;
-                    case 2: Lay(a); break;
-                    case 3: Xem(a); break;
-                    case 4: Duyet(a); break;
+                    case 1: input_student(a); break;
+                    case 2: duyet(a); break;
+                    case 3: output_student(a); break;
+                    case 4: suadiem(a); break;
+                    case 5: hocbong(a); break;
+                    case 6: xemdiem(a); break;
+                    case 7: sortdown(a); break;
+                    case 8: sortdown_name(a); break;
                     case 0: m = 0; break;
                     default: m = 0; break;
+                    
+                }
+            } while (m >= 1 && m <= 8);
+        }
+        public static void input_student(ArrayList a)
+        {
+            Console.WriteLine("Ho hoc sinh");
+            string f_name = Console.ReadLine();
+            Console.WriteLine("Ten lot hoc sinh");
+            string s_name = Console.ReadLine();
+            Console.WriteLine("Ten hoc sinh");
+            string p_name = Console.ReadLine();
+            Console.WriteLine("Ma so hoc sinh");
+            string code = Console.ReadLine();
+            Console.WriteLine("Tuoi hoc sinh");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Diem trung binh");
+            double mark = Convert.ToDouble(Console.ReadLine());
+            a.Add(new students(code,f_name,s_name,p_name,age,mark));
+        }
+        public static void output_student(ArrayList a)
+        {
+            Console.Write("Nhap ten sinh vien: ");
+            string b = Console.ReadLine();
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (((students)a[i]).p_name.ToLower().Contains(b.ToLower()))
+                {
+                    ((students)a[i]).infor();
                 }
             }
-            while (m >= 1 && m <= 5);
         }
-        static void Add(Queue<int> a)
+        public static void duyet(ArrayList a)
         {
-            int b = Convert.ToInt32(Console.ReadLine());
-            a.Enqueue(b);
+            for(int i = 0; i<a.Count;i++)
+            {
+                ((students)a[i]).infor();
+            }    
         }
-        static void Lay(Queue<int> a)
+        public static void suadiem(ArrayList a)
         {
-            a.Dequeue();
+            Console.Write("Nhap ho ten sinh vien: ");
+            string b = Console.ReadLine();
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (((students)a[i]).p_name.ToLower().Contains(b.ToLower()))
+                {
+                    Console.Write("Sua diem sinh vien: ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(value: $"Diem truoc khi sua la {((students)a[i]).s_mark}");
+                    ((students)a[i]).s_mark = c;
+                    Console.WriteLine(value: $"Diem sau khi sua la {((students)a[i]).s_mark}");
+                    ((students)a[i]).infor();
+                }
+            }
         }
-        static void Xem(Queue<int> a)
+        public static void hocbong(ArrayList a)
         {
-            Console.WriteLine(a.Peek());
+            Console.WriteLine("Danh sach sinh vien nhan hoc bong la");
+            for(int i = 0; i< a.Count ; i++)
+            {
+                if(((students)a[i]).s_mark >=8)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine(((students)a[i]).name());
+                    Console.WriteLine(((students)a[i]).s_mark);
+                    Console.WriteLine("----------------------");
+                }
+            }
         }
-        static void Duyet(Queue<int> a)
+        public static void xemdiem(ArrayList a)
         {
-            foreach(int b in a)
-                Console.WriteLine(b);
+            for (int i = 0; i < a.Count; i++)
+            {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine(((students)a[i]).name());
+                    Console.WriteLine(((students)a[i]).s_mark);
+                    Console.WriteLine("----------------------");
+            }
         }
-     
-    } 
+        public static void sortdown(ArrayList a)
+        {
+            ArrayList replace = new ArrayList();
+            for(int i =0; i< a.Count; i++)
+            {
+                replace.Add(i);
+            }
+            for(int i = 0; i<a.Count;i++)
+            {
+                for(int j = i+1; j<a.Count; j++)
+                {
+                    if(((students)a[i]).s_mark >= ((students)a[j]).s_mark)
+                            
+                      {
+                        replace[i] = a[i];
+                        a[i] = a[j];
+                        a[j] = replace[i];
+
+                      }
+                }
+            }
+            for (int i = 0; i < a.Count; i++)
+            {
+                ((students)a[i]).infor();
+            }
+        }
+        public static void sortdown_name(ArrayList a)
+        {
+            ArrayList replace = new ArrayList();
+            for (int i = 0; i < a.Count; i++)
+            {
+                replace.Add(i);
+            }
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    if (((students)a[i]).s_mark >= ((students)a[j]).s_mark)
+
+                    {
+                        replace[i] = a[i];
+                        a[i] = a[j];
+                        a[j] = replace[i];
+
+                    }
+                }
+            }
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    if (((students)a[i]).s_mark >= ((students)a[j]).s_mark)
+                    {
+                        if (((students)a[i]).p_name[0] > ((students)a[j]).p_name[0])
+                        {
+                            replace[i] = a[i];
+                            a[i] = a[j];
+                            a[j] = replace[i];
+                            break;
+                        }
+                        else if (((students)a[i]).p_name[1] > ((students)a[j]).p_name[1])
+                        {
+                            replace[i] = a[i];
+                            a[i] = a[j];
+                            a[j] = replace[i];
+                            break;
+                        }
+                        else if (((students)a[i]).p_name[2] > ((students)a[j]).p_name[2])
+                        {
+                            replace[i] = a[i];
+                            a[i] = a[j];
+                            a[j] = replace[i];
+                            break;
+                        }
+                        else if (((students)a[i]).p_name[3] > ((students)a[j]).p_name[3])
+                        {
+                            replace[i] = a[i];
+                            a[i] = a[j];
+                            a[j] = replace[i];
+                            break;
+                        }
+                        else
+                        {
+                            replace[i] = a[i];
+                            a[i] = a[j];
+                            a[j] = replace[i];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-        
-        
     
 
 
